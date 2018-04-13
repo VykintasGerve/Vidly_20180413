61c64c4271d73e3855dae70c2ab18abfb4eb809d
Vykintas Gervė <vykintas.gerve@gmail.com>
2018-04-13 15:13:30 +0300

    123

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,7 +12,15 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
+            /*
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" }, // @"2015|2016"
+                new { year = @"\d{4}", month = @"\d{2}"}
+            );
+            */
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        //[Route("customers/index")]
+        public ActionResult Index()
+        {
+
+            var customerList = new List<Customer>
+            {
+                new Customer{ Id = 1, Name = "John Smith" },
+                new Customer{ Id = 2, Name = "Mary Williams" },
+            };
+            var customers = new CustomersViewModel()
+            {
+                Id = 1,
+                Name = "Customers",
+                Customers = customerList
+
+            };
+            return View(customers);
+        }
+        public ActionResult Details(int? Id)
+        {
+            var customerList = new List<Customer>
+            {
+                new Customer{ Id = 1, Name = "John Smith" },
+                new Customer{ Id = 2, Name = "Mary Williams" },
+            };
+
+            if (!Id.HasValue)
+            {
+                return HttpNotFound();
+            }
+            else 
+            {
+                var customer = customerList.Find(c => c.Id == Id);
+                if (customer == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(customer);
+                }
+                    
+            }
+        }
+    }
+}
